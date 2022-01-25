a0564a9271206a87b0a24ce3d468330c679f23aa
joelmatiasduran <joelmatiasduran@gmail.com>
2022-01-25 17:44:29 -0300

    Fixes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,16 +10,22 @@ const Footer: React.FC = () => {
     <>
       <div className="">
         <footer className="text-white body-font bg-soft-blue">
-          <div className="bg-indigo-900">
-            <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+          <div className="bg-indigo-900 py-6">
+            <div className="container mx-auto px-5 flex flex-wrap flex-col sm:flex-row">
               <p className="text-white text-sm text-center sm:text-left">
                 <a
                   href="#"
                   rel="noopener noreferrer"
-                  className="text-2xl text-white ml-1 fill-white"
+                  className="p-4 text-2xl text-white ml-1 fill-white"
                   target="_blank"
                 >
-                  <Image src={Logo} alt="Navbar Logo" className="fill-white" />
+                  <Image
+                    src={Logo}
+                    alt="Navbar Logo"
+                    className="fill-white"
+                    width="200"
+                    height="35"
+                  />
                 </a>
               </p>
               <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
--- components/Newsletter.tsx
+++ components/Newsletter.tsx
@@ -7,11 +7,11 @@ const Newsletter: React.FC<NewsletterProps> = ({}) => {
     <>
       <div className="w-full bg-soft-blue text-white">
         <div className="container px-5 py-14 mx-auto flex md:items-center lg:items-center md:flex-col md:flex-nowrap flex-wrap flex-col">
-          <h3 className="py-6">35.000 + ALREADY JOINED</h3>
-          <h3 className="text-4xl font-semibold">
+          <h3 className="py-6 text-xl">35.000 + ALREADY JOINED</h3>
+          <h3 className="text-4xl font-semibold max-w-md text-center">
             Stay up-to-date with what we are doing
           </h3>
-          <div className="flex flex-row items-center justify-center pt-4">
+          <div className="flex flex-row items-center justify-center pt-8 pb-4">
             <input className="py-4 px-4 rounded-md text-black" />
             <button className="ml-4 px-8 py-4 text-white hover:text-red-400 bg-red-400 hover:bg-white border-2 border-transparent hover:border-red-400 rounded-lg duration-300">
               Contact Us
