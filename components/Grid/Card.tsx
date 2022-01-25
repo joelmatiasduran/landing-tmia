764acfd7bf6a0ab9fea7a26916e0559298ced3b1
joelmatiasduran <joelmatiasduran@gmail.com>
2022-01-25 01:34:28 -0300

    Hero images

--- components/Grid/Card.tsx
+++ components/Grid/Card.tsx
@@ -30,7 +30,7 @@ const Card: React.FC<CardProps> = ({
           </div>
           <div className="mt-12 flex flex-col">
             <Image src={Lines} alt="Lines" width="120" height="5" />
-            <button className="mt-12 px-8 py-4 bg-blue-600 rounded-lg text-lg text-white">
+            <button className="mt-12 px-8 py-4 bg-blue-600 hover:bg-white border-4 border-transparent hover:border-blue-600 rounded-lg text-lg text-white hover:text-blue-600 duration-300">
               Add & Install Extension
             </button>
           </div>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,32 +1,38 @@
-import React from "react";
-
+import Image from "next/image";
+import HeroImage from "../public/images/illustration-hero.svg";
 // interface HeroProps {
 
 // }
-
 const Hero: React.FC = ({}) => {
   return (
     <>
       <div className="flex flex-col items-center justify-center pt-14">
-        <div className="flex flex-row w-full">
+        <div className="flex flex-row">
           <div className="w-1/2">
-            <h1 className="text-4xl font-bold">A Simple Bookmark Manager</h1>
-            <p className="mt-6">
+            <h1 className="text-5xl font-bold">A Simple Bookmark Manager</h1>
+            <p className="text-xl mt-6">
               A clean and simple interface to organize your favourite websites.
               Open a new browser tab and see your sites load instantly.Try it
               for free.
             </p>
             <div>
-              <button className="border-4 border-purple-500 px-8 py-4 rounded-sm">
+              <button className="border-4 text-white bg-blue-500 px-8 py-4 rounded-lg">
                 Get it on Chrome
               </button>
-              <button className="border-4 border-black px-8 py-4 rounded-sm">
+              <button className="border-4 border-black px-8 py-4 rounded-lg">
                 Get it on Firefox
               </button>
             </div>
           </div>
           <div className="w-1/2">
-            <h1 className="text-4xl font-bold">A Simple Bookmark Manager</h1>
+            <div className="w-full relative flex flex-col-reverse mt-64">
+              <div className="absolute">
+                <Image src={HeroImage} alt="" width="800" height="600" />
+              </div>
+              <div className="ml-12">
+                <div className="bg-blue-700 h-60 flex-grow rounded-l-b-full rounded-l-full "></div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
