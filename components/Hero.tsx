1ffc51928b7b8a42a12eb1d5f5f6dde33545bffa
joelmatiasduran <joelmatiasduran@gmail.com>
2022-01-26 13:33:28 -0300

    White Footer

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,11 +6,11 @@ import HeroImage from "../public/images/illustration-hero.svg";
 const Hero: React.FC = ({}) => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center mt-24 pt-14 w-full min-w-full">
+      <div className="flex flex-col items-center justify-center mt-24 pt-14 w-full min-w-full ">
         <div className="flex flex-col md:flex-row w-full min-w-full">
           <div className="w-full md:w-3/6">
-            <div className="md:pl-44 pr-38 py-24">
-              <h1 className="text-5xl font-bold font-Rubik">
+            <div className="md:pl-44 pr-38 py-24 max-w-2xl">
+              <h1 className="text-5xl text-very-dark-blue font-bold font-Rubik">
                 A Simple Bookmark Manager
               </h1>
               <p className="text-xl mt-8">
