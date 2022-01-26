f0e227cccb505b906eaafa8b5770d5c12573dd61
joelmatiasduran <joelmatiasduran@gmail.com>
2022-01-26 00:58:56 -0300

    Features Bar

--- components/FAQ/index.tsx
+++ components/FAQ/index.tsx
@@ -1,28 +1,36 @@
-import React from "react";
+import { useState } from "react";
 
 interface indexProps {}
 
 const Faq: React.FC<indexProps> = ({}) => {
+  const [isOpen, setIsOpen] = useState<number>(0);
   return (
     <>
       <div className="flex flex-col items-center justify-center bg-white">
         <div className="flex flex-col items-center justify-center text center">
-          <h2 className="text-4xl font-bold">Frequently Asked Questions</h2>
+          <h2 className="text-4xl font-bold text-center">
+            Frequently Asked Questions
+          </h2>
           <h6 className="pt-8 pb-10 text-xl max-w-2xl text-center text-gray-500">
             Here are some of our FAQs. If you have any other questions you’d
             like answered please feel free to email us.
           </h6>
         </div>
-        <div className="flex flex-col justify-center items-center max-w-2xl">
-          <div className="border-t border-gray-300 py-4 pb-6">
-            <div className="flex flex-row justify-between">
-              <h2 className="text-xl">What is Bookmark?</h2>
+        <div className="flex flex-col justify-center items-center max-w-2xl w-full">
+          <div className="border-t border-gray-300 py-4 pb-4 w-full">
+            <div className="flex flex-row justify-between w-full min-w-full">
+              <h2 className="flex-grow text-xl">What is Bookmark?</h2>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-8 w-8 font-bold text-soft-red"
+                className={`h-8 w-8 font-bold ${
+                  isOpen === 1
+                    ? "text-soft-red rotate-180 duration-300"
+                    : "text-soft-blue duration-300"
+                } cursor-pointer`}
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
+                onClick={() => setIsOpen(2)}
               >
                 <path
                   strokeLinecap="round"
@@ -32,37 +40,122 @@ const Faq: React.FC<indexProps> = ({}) => {
                 />
               </svg>
             </div>
-            <h5 className="mt-10 text-xl text-gray-400">
+            <h5
+              className={
+                isOpen
+                  ? "mt-10 text-xl text-gray-400 duration-300"
+                  : "delay-300 h-0 hidden duration-300"
+              }
+            >
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
               tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum
               quis quam ornare mattis.
             </h5>
           </div>
-          <div className="border-t border-black py-4">
-            <div>
-              <h2 className="text-xl">What is Bookmark?</h2>
+          <div className="border-t border-gray-300 py-4 pb-4 w-full">
+            <div className="flex flex-row justify-between w-full min-w-full">
+              <h2 className="flex-grow text-xl">
+                How can I request a new browser?
+              </h2>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className={`h-8 w-8 font-bold ${
+                  isOpen === 1
+                    ? "text-soft-red rotate-180 duration-300"
+                    : "text-soft-blue duration-300"
+                } cursor-pointer`}
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                onClick={() => setIsOpen(1)}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 15l7-7 7 7"
+                />
+              </svg>
             </div>
-            <h5 className="mt-10 text-lg">
+            <h5
+              className={
+                isOpen
+                  ? "mt-10 text-xl text-gray-400 duration-300"
+                  : "delay-300 h-0 hidden duration-300"
+              }
+            >
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
               tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum
               quis quam ornare mattis.
             </h5>
           </div>
-          <div className="border-t border-black py-4">
-            <div>
-              <h2 className="text-xl">What is Bookmark?</h2>
+          <div className="border-t border-gray-300 py-4 pb-4 w-full">
+            <div className="flex flex-row justify-between w-full min-w-full">
+              <h2 className="flex-grow text-xl">Is there a mobile app?</h2>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className={`h-8 w-8 font-bold ${
+                  isOpen === 3
+                    ? "text-soft-red rotate-180 duration-300"
+                    : "text-soft-blue duration-300"
+                } cursor-pointer`}
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                onClick={() => setIsOpen(3)}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 15l7-7 7 7"
+                />
+              </svg>
             </div>
-            <h5 className="mt-10 text-lg">
+            <h5
+              className={
+                isOpen
+                  ? "mt-10 text-xl text-gray-400 duration-300"
+                  : "delay-300 h-0 hidden duration-300"
+              }
+            >
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
               tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum
               quis quam ornare mattis.
             </h5>
           </div>
-          <div className="border-t border-black py-4">
-            <div>
-              <h2 className="text-xl">What is Bookmark?</h2>
+          <div className="border-t border-gray-300 py-4 pb-4 w-full">
+            <div className="flex flex-row justify-between w-full min-w-full">
+              <h2 className="flex-grow text-xl">
+                What about other Chromium Browsers?
+              </h2>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className={`h-8 w-8 font-bold ${
+                  isOpen === 4
+                    ? "text-soft-red rotate-180 duration-300"
+                    : "text-soft-blue duration-300"
+                } cursor-pointer`}
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                onClick={() => setIsOpen(4)}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 15l7-7 7 7"
+                />
+              </svg>
             </div>
-            <h5 className="mt-10 text-lg">
+            <h5
+              className={
+                isOpen
+                  ? "mt-10 text-xl text-gray-400 duration-300"
+                  : "delay-300 h-0 hidden duration-300"
+              }
+            >
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
               tincidunt justo eget ultricies fringilla. Phasellus blandit ipsum
               quis quam ornare mattis.
--- components/Features/index.tsx
+++ components/Features/index.tsx
@@ -3,16 +3,23 @@ import { useState } from "react";
 interface indexProps {}
 
 const Features: React.FC<indexProps> = ({}) => {
+  const [isSelected, setIsSelected] = useState<string>("");
+
   return (
     <>
-      <div className="flex flex-col justify-center items-center mt-8">
+      <div className="flex flex-col justify-center items-center mt-8 w-full">
         <div>
           <h2 className="text-4xl font-bold text-center">Features</h2>
-          <h4 className="pt-8 text-xl text-gray-500 max-w-2xl text-center">
+          <h4 className="pt-8 text-2xl text-gray-500 max-w-2xl text-center">
             Our aim is to make it quick and easy for you to access your
             favourite websites. Your bookmarks sync between your devices so you
             can access them on the go
           </h4>
+        </div>
+        <div className="mt-16 flex flex-row text-2xl justify-between border-2 border-b-black py-5">
+          <h4 className="px-6">Simple Bookmarking</h4>
+          <h4 className="px-6">Speedy Searching</h4>
+          <h4 className="px-6">Easy Sharing</h4>
         </div>
       </div>
     </>
