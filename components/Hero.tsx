8bc64b14984a12369c0ce31e64fd357ee222dbd9
joelmatiasduran <joelmatiasduran@gmail.com>
2022-01-26 17:31:57 -0300

    Mobile Fixes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,18 +7,18 @@ const Hero: React.FC = ({}) => {
   return (
     <>
       <div className="flex flex-col items-center justify-center mt-24 pt-14 w-full min-w-full ">
-        <div className="flex flex-col md:flex-row w-full min-w-full">
+        <div className="flex flex-col-reverse md:flex-row w-full min-w-full">
           <div className="w-full md:w-3/6">
             <div className="md:pl-44 pr-38 py-24">
-              <h1 className="text-6xl text-very-dark-blue font-bold font-Rubik max-w-3xl">
+              <h1 className="text-4xl md:text-6xl text-very-dark-blue font-bold font-Rubik w-full max-w-lg md:max-w-3xl text-center md:text-left">
                 A Simple Bookmark Manager
               </h1>
-              <p className="text-2xl mt-8 max-w-4xl text-gray-400">
+              <p className="text-2xl mt-8 max-w-4xl text-gray-400 text-center md:text-left">
                 A clean and simple interface to organize your favourite
                 websites. Open a new browser tab and see your sites load
                 instantly.Try it for free.
               </p>
-              <div className="mt-12 ml-2">
+              <div className="mt-12 ml-2 flex items-center justify-center md:justify-start">
                 <button className="mr-4 border-4 text-white bg-blue-500 px-8 py-4 rounded-lg">
                   Get it on Chrome
                 </button>
