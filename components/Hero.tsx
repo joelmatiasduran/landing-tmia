2d6b390bd4ab9403ad058b3ecdb5a7dc2f09e85d
joelmatiasduran <joelmatiasduran@gmail.com>
2022-01-25 22:27:52 -0300

    Mobile fixes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,9 +7,9 @@ const Hero: React.FC = ({}) => {
   return (
     <>
       <div className="flex flex-col items-center justify-center pt-14 w-full min-w-full">
-        <div className="flex flex-row w-full min-w-full">
-          <div className="w-1/2">
-            <div className="px-44 py-24">
+        <div className="flex flex-col md:flex-row w-full min-w-full">
+          <div className="w-full md:w-1/2">
+            <div className="md:px-44 py-24">
               <h1 className="text-5xl font-bold font-Rubik">
                 A Simple Bookmark Manager
               </h1>
@@ -28,7 +28,7 @@ const Hero: React.FC = ({}) => {
               </div>
             </div>
           </div>
-          <div className="w-1/2">
+          <div className="w-full md:w-1/2">
             <div className="w-full relative flex flex-col-reverse mt-64">
               <div className="absolute">
                 <Image
